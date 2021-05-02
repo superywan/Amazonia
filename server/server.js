28b8f3fcda7c8f889ad1fb39f90afcdd63ff3f5c
Eddy Yi <superywan@Eddys-MacBook-Pro.local>
2021-05-02 18:25:46 +0900

    Heroku Fixing Error

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 import path from "path";
 import express from "express";
 import dotenv from "dotenv";
+import colors from "colors";
 import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
